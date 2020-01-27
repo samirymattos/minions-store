7f370574f4e07ad8069f2ee33d2b5bba42dba843
samiry <samirymattos@id.uff.br>
2020-01-26 21:43:53 -0300

    efeitos 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import initMenuMobile from "./modules/menu-mobile.js";
 import initOutsideClick from "./modules/outsideclick.js";
+import initScrollSuave from "./modules/scroll-suave";
 
 initMenuMobile();
 initOutsideClick();
+initScrollSuave();
